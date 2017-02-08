2e000730ed2b49beb96e86fbafa825fbab3b46c5
Pvtwuyver <pvtwuyver@gmail.com>
2017-02-08 22:27:45 +0100

    working version

--- homework/week1/Scraping/tvscraper.py
+++ homework/week1/Scraping/tvscraper.py
@@ -18,7 +18,12 @@ TARGET_URL = "http://www.imdb.com/search/title?num_votes=5000,&sort=user_rating,
 BACKUP_HTML = 'tvseries.html'
 OUTPUT_CSV = 'tvseries.csv'
 
-scraperesults = []
+# extra function to convert titles,genres and actornames to UTF-8
+# based on code on https://docs.python.org/2/library/csv.html
+def convert_unicode(unicode_txt):
+    'Returns a UTF-8 encoded version of the Unicode input string'
+    return unicode_txt.encode('utf-8')
+
 def extract_tvseries(dom):
     '''
     Extract a list of highest rated TV series from DOM (of IMDB page).
@@ -30,62 +35,62 @@ def extract_tvseries(dom):
     - Actors/actresses (comma separated if more than one)
     - Runtime (only a number!)
     '''
+    scraperesults = []
 
     url = URL("http://www.imdb.com/search/title?num_votes=5000,&sort=user_rating,desc&start=1&title_type=tv_series")
     dom = DOM(url.download(cached=True))
     
-    serie = []
-    titles = []
-    ratings = []
-    runtimes = []
-    genres = []   
-    actors = []
     # get all serie titles
     for e in dom.by_tag("div.pagecontent"): 
+        # mainloop over all series
         for a in e.by_tag("div.lister-item mode-advanced"): 
+            # create serie-dicionary with Key fieldnames
+            serie = {}
             for d in a.by_tag("div.lister-item-content"): 
                 for c in d.by_tag("h3.lister-item-header"):
                     for f in c.by_tag("a."):
                         title = plaintext(f.content)
-                        titles.append(title)
-                        serie.append(title)
+                        serie["Title"]= convert_unicode(title)
                 # get corresponding ratings
                 for h in d.by_tag("div.ratings-bar"):
                     for i in h.by_tag("div.inline-block ratings-imdb-rating"):
                         rating = plaintext(i.content)
-                        ratings.append(rating)
-                        serie.append(rating)
+                        serie["Rating"]= rating
                 # get genres and runtime
                 for j in d.by_tag("p.text-muted"):
                     for k in j.by_tag("span.genre"):
                         genre = plaintext(k.content)
-                        genres.append(genre) 
-                        serie.append(genre)
+                        serie["Genre"]= convert_unicode(genre)
                     for m in j.by_tag("span.runtime"):
                         runtime = plaintext(m.content)
-                        # remove last 4 characters ( min)
+                        # remove last 4 characters (=minutes)
                         runtime = runtime[:-4]
-                        runtimes.append(runtime)
-                        serie.append(runtime)
+                        serie["Runtime"]= runtime
                 # get Actors
+                actors = []
                 for l in d.by_tag("p.")[2].by_tag("a"):
                         actor = plaintext(l.content)
                         actors.append(actor)
-                        serie.append(actor)
+                # comma seperate actors in one field
+                serie["Actors"]= convert_unicode(", ".join(actors))
 
-    scraperesults.append(serie)
+            # create scraperesults dictionary
+            scraperesults.append(serie)
 
-    print scraperesults
-    return [scraperesults]  
+    return scraperesults  
 
 
 def save_csv(f, tvseries):
     '''
     Output a CSV file containing highest rated TV-series.
     '''
-    writer = csv.writer(f)
-    writer.writerow(['Title', 'Rating', 'Genre', 'Actors', 'Runtime'])
-    writer.writerows(scraperesults)
+    # based on code on https://docs.python.org/2/library/csv.html
+    fieldnames = ['Title', 'Rating', 'Genre', 'Actors', 'Runtime']
+    writer = csv.DictWriter(f, fieldnames=fieldnames)
+    writer.writeheader()
+    # loop through dictionary
+    for row in tvseries:
+        writer.writerow(row)
 
 if __name__ == '__main__':
     # Download the HTML file
